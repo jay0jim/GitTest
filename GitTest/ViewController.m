bafb70e825944b4ed5554e09d4ec4d511eac5d95
Tony <284096905@qq.com>
2015-07-22 22:20:15 +0800

    Commit before merging branch....

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello, world");
+    
+    NSLog(@"Merge branch....");
 }
 
 - (void)didReceiveMemoryWarning {
